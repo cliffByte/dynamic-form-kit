f95825b0400ad4fee9b3d8a575654a472325e44f
adkshishir <adhikarishishir50@gmail.com>
2026-04-28 14:24:24 +0545

    feat: create index file to export runtime components for form management

---
+++ src/components/runtime/index.ts
@@ -0,0 +1,9 @@
+export { FormRenderer } from './FormRenderer';
+export type { FormRendererProps } from './FormRenderer';
+
+export { SubmissionViewer } from './SubmissionViewer';
+export type { SubmissionViewerProps } from './SubmissionViewer';
+
+export { SubmissionEditor } from './SubmissionEditor';
+export type { SubmissionEditorProps } from './SubmissionEditor';
+
